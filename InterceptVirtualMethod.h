ac1583e34b1704a43bf679610c4a66109578906c
Alex Marmer <amarmer@gmail.com>
2023-01-17 14:50:27 -0800

    Update InterceptVirtualMethod.h

--- InterceptVirtualMethod.h
+++ InterceptVirtualMethod.h
@@ -15,24 +15,28 @@ struct VTable<0> {
 // Assume that there is no more than 50 virtual functions in a class.
 using MaxVTable = VTable<50>;
 
-template <typename T> 
+template <typename T>
 int GetVTableSize() {
   struct VTableSizeDetector : T {
     virtual int GetSize(void* p = nullptr) { return -1; }
   };
 
-  return reinterpret_cast<VTableSizeDetector*>(&MaxVTable())->GetSize();
+  MaxVTable maxVTable;
+
+  return reinterpret_cast<VTableSizeDetector*>(&maxVTable)->GetSize();
 };
 
 template <typename T, typename TRet, typename... Args>
-int GetVTableIndex(TRet(T::*f)(Args...)) {
+int GetVTableIndex(TRet(T::* f)(Args...)) {
   auto fGetIndex = reinterpret_cast<int (T::*)(void*)>(f);
 
-  return (reinterpret_cast<T*>(&MaxVTable())->*fGetIndex)(nullptr);
+  MaxVTable maxVTable;
+
+  return (reinterpret_cast<T*>(&maxVTable)->*fGetIndex)(nullptr);
 }
 
 template <typename T, typename TF, typename TRet, typename... Args>
-TRet (STDMETHODCALLTYPE *OverrideVTableFunction(T* pT, TRet(TF::*fOrig)(Args...), TRet(*fNew)(T*, Args...)))(T*, Args...) {
+TRet (*OverrideVTableFunction(T* pT, TRet(TF::* fOrig)(Args...), TRet(*fNew)(T*, Args...)))(T*, Args...) {
   // Check that T is derived from TF
   TF* p = pT;
 
@@ -59,20 +63,20 @@ TRet (STDMETHODCALLTYPE *OverrideVTableFunction(T* pT, TRet(TF::*fOrig)(Args...)
 
   DWORD oldProtect = 0;
   if (!::VirtualProtect(pVTable->pVTableFunctions + indx, sizeof(VTableFunction), PAGE_EXECUTE_READWRITE, &oldProtect)) {
-      // Log Error
-      return nullptr;
+    // Log Error
+    return nullptr;
   }
 
   *(pVTable->pVTableFunctions + indx) = reinterpret_cast<VTableFunction>(fNew);
 
   if (!::VirtualProtect(pVTable->pVTableFunctions + indx, sizeof(VTableFunction), oldProtect, &oldProtect)) {
-      // Log Error
+    // Log Error
   }
 
   return fOld;
 }
 
 template <typename T, typename TRet, typename... Args>
-TRet(*VTableFunctionType(TRet(T::*fOrig)(Args...)))(T*, Args...);
+TRet(*VTableFunctionType(TRet(T::* fOrig)(Args...)))(T*, Args...);
 
 #define VTABLE_FUNCTION_TYPE(F) decltype(VTableFunctionType(F))
